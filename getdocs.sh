826867692d0b17589136eb143fd574326e2a9c5e
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-11-25 17:16:20 +0000

    Update getdocs.sh

--- getdocs.sh
+++ getdocs.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 echo "Installing Needed Applications"
-apt-get install doxygen python3 python3-pip python3-setuptools 
+apt-get install doxygen python3 python3-pip python3-setuptools
 pip3 install mkdocs-material
 echo "Done"
 echo "Cloning Repository"
 git config --global user.email "admin@derangedsenators.me"
 git config --global user.name "buildbot"
+git clone -b gh-pages https://$BUILD_BOT_SECRET@github.com/derangedsenators/website.git > /dev/null 2>&1
+cd website
 echo "Removing old Documentation"
 rm -r Documentation/
-rm -r website/
-echo "recursive website removed"
+echo "Getting Documentations"
+echo "Getting PlayerLink"
+mkdir codedoc
+cd codedoc
+git submodule add https://$BUILD_BOT_SECRET@github.com/derangedsenators/playerlink.git > /dev/null 2>&1
+echo "Getting Cops and Robbers"
+git submodule add https://$BUILD_BOT_SECRET@github.com/derangedsenators/copsandrobbers.git > /dev/null 2>&1
 echo "Done... Building Doxygen Documentation"
-doxygen Doxyfile
+doxygen ../Doxyfile
 echo "Done... Converting to Markdown"
-mkdir docs/
-doxygen/doxybook2 --input xml/ --output docs/ --config doxygen/doxybookcfg.json
+mkdir ../docs/
+./../doxygen/doxybook2 --input xml/ --output ../docs/ --config ../doxygen/doxybookcfg.json
 echo "Done... Building Site with MKDOWN-material"
 cd ..
 rm -r docs/Files
