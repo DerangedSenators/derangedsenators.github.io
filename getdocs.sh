29b757022e94330dfbfed0a6dc4b7fb3d604ff44
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-11-25 12:19:11 +0000

    Get the submodule with buildbot

--- getdocs.sh
+++ getdocs.sh
@@ -8,9 +8,9 @@ echo "Getting Documentations"
 echo "Getting PlayerLink"
 mkdir codedoc
 cd codedoc
-git submodule add https://github.com/derangedsenators/playerlink.git
+git submodule add https://admin@derangedsenators.me:$1@github.com/derangedsenators/playerlink.git
 echo "Getting Cops and Robbers"
-git submodule add https://github.com/derangedsenators/copsandrobbers.git
+git submodule add https://admin@derangedsenators.me:$1@github.com/derangedsenators/copsandrobbers.git
 echo "Done... Building Doxygen Documentation"
 doxygen ../Doxyfile
 echo "Done... Converting to Markdown"
