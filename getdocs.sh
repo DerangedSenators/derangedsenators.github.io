db455bc6eb6216ac5f85752b84a5d78c1713876a
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-12-02 21:05:07 +0000

    GetDocs allow local documentation

--- getdocs.sh
+++ getdocs.sh
@@ -38,6 +38,8 @@ rm -r docs/PlayerLink/Files
 rm -r docs/PlayerLink/Pages
 rm -r docs/"Cops And Robbers"/Files
 rm -r docs/"Cops And Robbers"/Pages
+mv -v codedoc/playerlink/docs/* docs/PlayerLink/
+mv -v codedoc/copsandrobbers/docs/* docs/"Cops And Robbers"/
 cp -R doxygen/overlays/. docs/
 mkdocs build --site-dir Documentation
 echo "Cleaning up"
