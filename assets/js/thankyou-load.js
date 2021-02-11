5324195e6ca5e72000149bac162f9e1419ec3da0
hsravat-4590 <hanzalah.ravat@gmail.com>
2021-02-11 17:14:40 +0000

    Deploying to gh-pages from  @ 0422afbef53fc4f8bfc38f77d326e86f7ee908ea 🚀

---
+++ assets/js/thankyou-load.js
@@ -0,0 +1,22 @@
+
+//Script for loading the thanks for downloading script
+import {downloadStandalone,downloadLauncher,downloadDynamic,getMainDLType} from './download.js'
+window.onload = (event) => {
+    const queryString = window.location.search;
+    console.log(queryString);
+    if (queryString === "") {
+        console.log("Its Empty")
+        downloadDynamic();
+    }
+    const urlParams = new URLSearchParams(queryString);
+    const dlType = urlParams.get('type');
+    const platform = urlParams.get('platform');
+    switch (dlType) {
+        case 'standalone':
+            downloadStandalone(platform);
+            break;
+        case 'launcher':
+            downloadLauncher(platform);
+            break;
+    }
+}
