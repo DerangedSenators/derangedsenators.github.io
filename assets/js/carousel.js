b528963bc6cf398022fd44623949f6889220b2da
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-02-11 21:52:03 +0000

    Replace Bootstrap Carousel with Slick Carousel

---
+++ assets/js/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.carousel-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots:true,
+        fade:true,
+        cssEase:'linear'
+    })
+});
